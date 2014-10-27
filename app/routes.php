1a0689581a8316f2f858a747c4e9f6d0e8538686
Mary Murphy <marymurphy@g.harvard.edu>
2014-10-27 11:45:13 -0400

    adding test routes

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,60 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('/books', function() {
+    return 'Here are all the books...';
+}); 
+
+Route::get('/books/{category}', function($category) {
+        return 'Here are all the books in the category of '.$category;
+}); 
+
+Route::get('/new', function() {
+
+    $view  = '<form method="POST">';
+    $view .= 'Title: <input type="text" name="title">';
+    $view .= '<input type="submit">';
+    $view .= '</form>';
+    return $view;
+
+});
+
+Route::post('/new', function() {
+
+    $input =  Input::all();
+    print_r($input);
+
+});
+
+Route::get('/practice', function() {
+
+    echo 'Hello World!';
+
+});
+
+Route::get('/get-environment',function() {
+
+    echo "Environment: ".App::environment();
+
+});
+
+Route::get('/trigger-error',function() {
+
+    # Class Foobar should not exist, so this should create an error
+    $foo = new Foobar;
+
+});
+
+Route::get('mysql-test', function() {
+
+    # Print environment
+    echo 'Environment: '.App::environment().'<br>';
+
+    # Use the DB component to select all the databases
+    $results = DB::select('SHOW DATABASES;');
+
+    # If the "Pre" package is not installed, you should output using print_r instead
+    echo Pre::render($results);
+
+});
